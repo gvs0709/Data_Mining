448d20bebe6c3af176e1dc3883bb7e2662fe8b7c
gvs0709 <gvillaress@yahoo.com.br>
2017-03-27 18:27:40 -0300

    Add files via upload

--- redeNeural.py
+++ redeNeural.py
@@ -43,17 +43,31 @@ class RedeNeural:
         seja alcançado
         '''
         
-        self.eta=eta #learning rate
+        self.eta=0.1 #learning rate inicial
         self.hidden_nodes=hidden_nodes #Numero de perceptrons (nós) na hidden layer
         
     def fit(self, X, y):
         pass
+        #This is the main training loop. The output shows the evolution of the error between the model and desired. The error steadily decreases. 
+ 
+        for j in xrange(60000): #training step
+            #Calculate forward through the network.
+            l0=X
+            l1=nonlin(np.dot(l0, syn0))
+            l2=nonlin(np.dot(l1, syn1))
+             
+            l2_error=y - l2 #Back propagation of errors using the chain rule.
 
+            if(j%10000)==0: #Only print the error every 10000 steps, to save time and limit the amount of output. 
+                print("Error: " + str(np.mean(np.abs(l2_error))))        
         
-        '''
-        o fit deve descobrir a dimensão do problema
-        e o número de classes.
-        '''
+            l2_delta=l2_error*nonlin(l2, deriv=True)
+            l1_error=l2_delta.dot(syn1.T)
+            l1_delta=l1_error * nonlin(l1,deriv=True)
+            
+            #update weights (no learning rate term)
+            hidden_nodes(1)+=l1.T.dot(l2_delta)
+            hidden_nodes(0)+=l0.T.dot(l1_delta)
         
     def predict(self, X):
         pass
