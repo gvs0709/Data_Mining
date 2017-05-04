daf2bd56e52538db0d5d131bba89367792335e46
gvs0709 <gvillaress@yahoo.com.br>
2017-05-04 18:27:23 -0300

    Add files via upload

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -19,31 +19,31 @@ y=mnist.target
 (l, c)=X.shape
 
 amostraTreino=int(l*0.3) #Porcentagem da entrada a ser usada para treino
-#amostraTeste=int(l*0.7) #Porcentagem da entrada a ser usada para teste
+amostraTeste=int(l*0.7) #Porcentagem da entrada a ser usada para teste
 
 treinoX=np.zeros((amostraTreino, c)) #Matriz de treino, inicializada com zeros
 treinoY=np.zeros((amostraTreino, 1)) #Matriz de treino, inicializada com zeros
 
-testeX=np.zeros((2800, c)) #Matriz de teste, inicializada com zeros
-testeY=np.zeros((2800, 1)) #Matriz de teste, inicializada com zeros
+testeX=np.zeros((amostraTeste, c)) #Matriz de teste, inicializada com zeros
+testeY=np.zeros((amostraTeste, 1)) #Matriz de teste, inicializada com zeros
 
 for i in xrange(amostraTreino):
 	k=np.random.randint(1, l) #Seleciona linhas aleatorias de X para treino
 	treinoX[i]=X[k] #Coloca a linha selecionada na matriz de treino
 	treinoY[i]=y[k] #Coloca a classe correspondente da linha escolhida na matriz 
 
-for i in xrange(2800):
+for i in xrange(amostraTeste):
 	k=np.random.randint(1, l) #Seleciona linhas aleatorias de X para teste
 	testeX[i]=X[k] #Coloca a linha selecionada na matriz de teste
 
 #print testeX
 #print X
 
-#--------------------------------------Metodos------------------------------------------#
+#--------------------------------------Metodos---------------------------------------------#
 #clf=MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 #clf=svm.SVC(kernel="linear")
 clf=tree.DecisionTreeClassifier()
-#----------------------------------------//---------------------------------------------#
+#----------------------------------------//------------------------------------------------#
 
 clf.fit(treinoX, treinoY)
 y_pred=clf.predict(X)
